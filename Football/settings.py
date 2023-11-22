8006e4d8f4ed626db14706018d24051dfd12db87
dilmurod1701 <139139677+dilmurod1701@users.noreply.github.com>
2023-11-22 18:17:25 +0500

    Updated: settings.py

--- Football/settings.py
+++ Football/settings.py
@@ -88,8 +88,11 @@ WSGI_APPLICATION = 'Football.wsgi.app'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'football',
+        'USER': 'default',
+        'PASSWORD': 'B8U0jDSGAdCv',
+        'HOST': 'ep-lively-wildflower-54512986.us-east-2.aws.neon.tech',
     }
 }
 
